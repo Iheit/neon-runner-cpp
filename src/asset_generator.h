cb1275a218605a53c0ebae66cfdecd2cb5a7664f
Iheit <112427152+Iheit@users.noreply.github.com>
2026-08-28 04:25:50 +0330

    Format asset generator header

--- src/asset_generator.h
+++ src/asset_generator.h
@@ -1,5 +1,8 @@
 #pragma once
+
 #include <functional>
 #include <string>
 
-bool GenerateAssets(const std::string& root, const std::function<void(int, const std::string&)>& progress);
+bool GenerateAssets(
+    const std::string& root,
+    const std::function<void(int, const std::string&)>& progress);
